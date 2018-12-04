ad2fb800378f8e251ccd923399b340d04aec1a40
shimajima-eiji <shimajima-eiji@live.jp>
2018-12-04 19:55:31 +0900

    [add] fix wfile.(codec_open -> open_file) ,salvage wdate.py, TODO scp -r

--- VSCode/SaveandRun.sh
+++ VSCode/SaveandRun.sh
@@ -1,23 +1,36 @@
 #!/usr/bin/env sh
 ### TODO use wssh.py for .py
 fromfile=$1
+if [ "$(basename ${fromfile})" = 'SaveandRun.sh' ];then
+  exit 0
+fi
+
+server='okamoto1'
+pass=''
 
 ### option
 if [ ! "$(echo ${fromfile} | grep .py$)" = '' ]; then
-    pyformat -i ${fromfile}
-    isort ${fromfile}
-    cmd='python3'
+  pyformat -i ${fromfile}
+  isort ${fromfile}
+  cmd='python3'
 elif  [ ! "$(echo ${fromfile} | grep .sh$)" = '' ]; then
-    cmd='bash'
+  cmd='bash'
 fi
 
-### convert from -> to
-from='/mnt/d/environment/Storage/OneDrive/WSL'
-to='/opt/src'
+if [ "$(hostname)" = 'DESKTOP-CO9ORBL' ]; then
+  ${cmd} ${fromfile}
+  exit 0
+fi
+
+to='~'  #to='/opt/src'
 tofile=$(echo ${fromfile} | sed "s#${from}#${to}#g")
 
 ### send(Required .ssh/config)
-server='server'
-pass='password'
-sshpass -p "${pass}" scp ${server} ${fromfile} ${tofile}
-sshpass -p "${pass}" ssh ${cmd} "${tofile}"
+sshpass=''
+if [ ! "${pass}" = '' ]; then
+  sshpass="sshpass -p ${pass}"
+fi
+echo "[ ${tofile} ]"
+${sshpass} ssh ${server} "mkdir -p $(dirname ${tofile})"
+${sshpass} scp ${fromfile} ${server}:${tofile}
+${sshpass} ssh ${server} "${cmd} ${tofile}"
