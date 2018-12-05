3720867921a43e2c5551074623415e8d94e50868
shimajima-eiji <shimajima-eiji@live.jp>
2018-12-05 15:16:30 +0900

    [add] forget updated file:path_convert.sh

---
+++ VSCode/path_convert.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# [TODO] #4 move to replace.sh -> ../../utils/snippet/shell/replace.sh
+path=$1
+local="${2:-$(hostname)}"  # No need?
+host="${3:-(hostname)}"  # No need?
+pass="${4:-(pass)}"  # No need?
+
+# replace path local tofile remote
+path=$(echo ${path} | sed -e "s#^/mnt/c#~#g")
+path=$(echo ${path} | sed -e "s#^/mnt/d#~#g")
+
+# for result
+echo "${path} ${local} ${host} ${pass}"
