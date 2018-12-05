5c29065286dc5fc3049239c7447ffaf18c95a9d3
shimajima-eiji <shimajima-eiji@live.jp>
2018-12-05 15:18:41 +0900

    [fix] path_convert.sh Required path

--- VSCode/path_convert.sh
+++ VSCode/path_convert.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 # [TODO] #4 move to replace.sh -> ../../utils/snippet/shell/replace.sh
+set -u
 path=$1
+set +u
+
 local="${2:-$(hostname)}"  # No need?
 host="${3:-(hostname)}"  # No need?
 pass="${4:-(pass)}"  # No need?
