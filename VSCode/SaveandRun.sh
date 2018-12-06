b7c7d2633900bb780ef67edb5a5050c5de619f18
shimajima-eiji <shimajima-eiji@live.jp>
2018-12-06 19:28:49 +0900

    [add] utils.ansible

--- VSCode/SaveandRun.sh
+++ VSCode/SaveandRun.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-### TODO #3 use wssh.py for .py
 cd $(dirname $0)
 
 fromfile=$1  # wslpath '${file}'
@@ -15,20 +14,24 @@ if [ ! "$(echo ${fromfile} | grep .py$)" = '' ]; then
   cmd='python3'
 elif  [ ! "$(echo ${fromfile} | grep .sh$)" = '' ]; then
   cmd='bash'
+elif  [ ! "$(echo ${fromfile} | grep .yaml$)" = '' ]; then
+  cmd='ansible-playbook -i /etc/ansible/project/hosts'
 else
   cmd=''
 fi
 
 ### convert from -> to
-path_convert="$(sh path_convert.sh ${fromfile})"
-tofile=$(echo ${path_convert} | cut -f1 -d' ')
-local=$(echo ${path_convert} | cut -f2 -d' ')
-host=$(echo ${path_convert} | cut -f3 -d' ')
-pass=$(echo ${path_convert} | cut -f4 -d' ')
+path_config="$(sh path_config.sh ${fromfile})"
+tofile=$(echo ${path_config} | cut -f1 -d' ')
+local=$(echo ${path_config} | cut -f2 -d' ')
+host=$(echo ${path_config} | cut -f3 -d' ')
+pass=$(echo ${path_config} | cut -f4 -d' ')
 
 ### for run to local
-if [ "$(hostname)" = "${local}" ]; then
-  ${cmd} ${fromfile}
+if [ "$(hostname)" = "${local}a" ]; then
+  if [ ! "${cmd}" = '' ]; then
+    ${cmd} ${fromfile}
+  fi
   exit 0
 fi
 
@@ -41,6 +44,7 @@ fi
 
 ${sshpass} ssh ${host} "mkdir -p $(dirname ${tofile})"
 ${sshpass} scp ${fromfile} ${host}:${tofile}
+echo "${fromfile} ${tofile}"
 echo "[complete] ${fromfile} -> ${host}:${tofile}"
 
 if [ ! "${cmd}" = '' ]; then
