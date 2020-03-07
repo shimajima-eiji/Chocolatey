a4701d6d221d4e531ce390070a8ef0f8facbe434
のむら屋ごろう <shimajima-eiji@live.jp>
2020-03-07 14:09:42 +0900

    Update WSL.sh

--- primitive/WSL.sh
+++ primitive/WSL.sh
@@ -15,16 +15,14 @@ echo '
 export PATH="$HOME/.anyenv/bin:$PATH"
 export PYENV_ROOT="$HOME/.anyenv/envs/pyenv"
 export PATH="${PYENV_ROOT}/shims:${PYENV_ROOT}/bin:${PATH}"
-'>>(shell)_profile
+' >> ~/.$(basename $SHELL)_profile  # 現在ログイン中のshellに書く。bashだと$SHELLは/bin/bash
 
 exec $SHELL -l
 
 # for pyenv
 
-
 # for python3.7
 sudo apt install -y build-essential libbz2-dev libdb-dev libreadline-dev libffi-dev libgdbm-dev liblzma-dev libncursesw5-dev libsqlite3-dev libssl-dev zlib1g-dev uuid-dev tk-dev
 anyenv install pyenv
 pyenv install ()  # (pyenv install --list)
-
 
