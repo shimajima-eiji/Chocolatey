bb6bfc7f5b533ada0603bc1dd49176881ca73e0a
のむら屋ごろう <shimajima-eiji@live.jp>
2020-07-28 23:34:03 +0900

    Update update_CHANGELOG.sh

--- wsl/update_CHANGELOG.sh
+++ wsl/update_CHANGELOG.sh
@@ -1,4 +1,5 @@
 # Refer: https://github.com/shimajima-eiji/Github_scout/wiki/【手引】更新履歴（CHANGELOG.md）
 echo $1
+echo "update"
 
 # github-changes -o (your account) -r (repository) --use-commit-body -t "更新履歴" -z Asia/Tokyo -m "YYYY年M月D日" -n 最終更新
