13c5663ec9b6f088c592ffd5ab5c6011037b24ab
のむら屋ごろう <shimajima-eiji@live.jp>
2020-08-03 10:22:28 +0900

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -4,12 +4,14 @@
 Get-ExecutionPolicy
 # 初期だとRestrictedとなっているはず
 Set-ExecutionPolicy AllSigned
+
 # 何か言われたらうざったいのでA
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-# ちまちまRしてたらうざったいのでA
 
 # お好みでどうぞ
-cinst (package-name or config-file)
+cinst (package-name or config-file) #(upgrade) で更新できる。chocolateyguiはこのコマンドでないとupgradeできない
 
 ## 途中、【Do you want to run the script?([Y]es/[A]ll - yes to all/[N]o/[P]rint):】と聞かれる事があるのでAとしておこう。
 
