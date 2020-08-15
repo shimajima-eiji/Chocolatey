d1749af00b90244ec7f1d1ff77fb96ec2729f622
のむら屋ごろう <shimajima-eiji@live.jp>
2020-08-15 23:03:41 +0900

    Update autopush.sh

--- wsl/git/autopush.sh
+++ wsl/git/autopush.sh
@@ -21,16 +21,18 @@ mkdir -p ${current}
 ### main
 cd ${current}
 message="[${today}][CHANGELOG] 最新化"
+branch_update_flag=false
 
 mkdir -p CHANGELOG
 for branch in ${@:3}; do
+  branch_update_flag=true
   git clone -b ${branch} git@github.com:${account}/${repository}.git
   cd ${repository}
   curl -sf ${sh_url} | sh -s -- ${repository} ${branch} true
   git add -A
   git commit -a -m "${message}"
   git tag -a v${today} -m "当日分の全コミット"
-  # git push origin v${today}
+  git push origin v${today}
   cd -
   rm -rf ${repository}
 
@@ -39,12 +41,15 @@ done
 
 git clone git@github.com:${account}/${repository}.git
 cd ${repository}
+if [ branch_update_flag ]; then
+  mv -f ../CHANGELOG CHANGELOG
+fi
 curl -sf ${sh_url} | sh -s -- ${repository} master false
 rm -rf CHANGELOG
 mv ../CHANGELOG CHANGELOG
 git add -A
 git commit -a -m "${message}"
 git tag -a v${today} -m "当日分の全コミット"
-# git push origin v${today}
+git push origin v${today}
 cd -
 rm -rf ${repository}
