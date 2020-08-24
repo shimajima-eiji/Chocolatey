4c700be5e9cc1744d5520cae28d0741305a9e688
のむら屋ごろう <shimajima-eiji@live.jp>
2020-08-24 17:24:46 +0900

    Update manual.py

--- wsl/cron/manual.py
+++ wsl/cron/manual.py
@@ -1,10 +1,40 @@
+"""
+# 概要
+cronで日常的に実施しているコマンドが実行されていない事に跡から気付いた時に、手動で一括実施するために作成
+ログイン中のユーザーの`crontab -l`の内容を時間順にソートして上からコマンドを実施する。
+
+# 使い方
+`curl https://raw.githubusercontent.com/shimajima-eiji/Chocolatey/master/wsl/cron/manual.py | python`
+
+0時0分からcronが動いていない事を想定しているので、たとえば10時まで動いていたような場合は本スクリプトを取得して
+`lists["date"] <= today]`の行を`(lists["date"] <= today & lists["date"] >= (datetime))` のように書き換える
+カッコ（）が最初と最後につくようになるので注意する
+
+# 注意点
+`pip install pandas`
+"""
+
+# python3
 import subprocess
 from subprocess import PIPE
+from datetime import datetime
+import pandas as pd
 
-with open('./cron') as f:
-  lines = [" ".join( line.split()[5:]) for line in f.readlines() if len(line.split()) > 5]
-  string="\nsleep 10\n".join(lines)
-  print(string)
+# crontabを抽出
+string="crontab -l"
+proc = subprocess.run(string, shell=True, stdout=PIPE, stderr=PIPE, text=True)
+lines = [line for line in proc.stdout.split("\n") if len(line.split()) > 5]
+commands = [" ".join( line.split()[5:]) for line in lines]
 
-  proc = subprocess.run(string, shell=True, stdout=PIPE, stderr=PIPE, text=True)
-  print('COMPLETE: {}'.format(proc.stdout))
+# crontabの現在時間までの内容を抽出
+today=datetime.today()
+times=[line.split()[1::-1] for line in lines]
+dates=[datetime(today.year, today.month, today.day, int(time[0]), int(time[1])) for time in times]
+
+# 時間順にソートしたいのでpandasにする
+def run(command):
+  proc = subprocess.run(command + "; sleep 10", shell=True, stdout=PIPE, stderr=PIPE, text=True)
+  print('COMPLETE: {}\nRESULT: {}'.format(command, proc.stdout))
+
+lists = pd.DataFrame({"date":dates,"command": commands})
+lists[lists["date"] <= today].sort_values("date")["command"].map(run)
