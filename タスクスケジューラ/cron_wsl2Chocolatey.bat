68cf81a840e4399806a0f45b9d378d5ecbe6af10
のむら屋ごろう <shimajima-eiji@live.jp>
2020-09-01 22:40:08 +0900

    [2020/09/01][chocolatey] 最新化、タスクスケジューラのパスを変更

--- タスクスケジューラ/cron_wsl2Chocolatey.bat
+++ タスクスケジューラ/cron_wsl2Chocolatey.bat
@@ -1,4 +1,5 @@
 @rem 日本語パスを扱う場合、文字コードはSJISにする必要がある。UTF8でもダメ。改行コードはCRLF
 @rem %cron_pathは事前に設定しておく
-@rem `SETX cron_path "(your path)"`
-copy /Y \\wsl$\Ubuntu-20.04\home\ubuntu\cron\cron %cron_path%
+@rem `SETX from_cron"(your path)"`
+@rem `SETX to_cron"(your path)"`
+copy /Y %from_cron% %to_cron%
